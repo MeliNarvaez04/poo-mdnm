2609d5b117e511b8915d504dfa215203cfd733bc
Lab A2 <mnarvaezm4@est.ups.edu.ec>
2024-01-29 11:33:47 -0500

    Add (II): Add PrintSquare Function

--- poo/src/Main.java
+++ poo/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printSquare(10);
+        System.out.println("---------------------");
+        printSquare(20);
+
+
+
+    }
+    public static void printSquare(int length){
+        for(int i = 1; i <= length; i++){
+            for (int m = 1; m <= length; m++){
+                System.out.print(" * ");
+            }
+            System.out.print("\n");//   es para separar \//
+            //System.out.print();
+        }
+
     }
 }
